2df0b5f499c4c604c7b2a5d321c3b985c7708730
Francisco Berti da Cruz <fbc991@live.com>
2023-03-21 21:40:46 -0300

    projeto finalizado

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -6,78 +6,53 @@ const Formulario = () => {
 
     const [altura, setAltura] = useState(0)
     const [peso, setPeso] = useState(0)
+    const [IMC, setIMC] = useState(0)
 
-    const calcularIMC = () => {
-        const IMC = ((peso / Math.pow(altura, 2)) * 100).toFixed(2)
-
-        if (IMC < 0.185) {
-            return (
-                <div>
-                    <p>O seu Indice de Massa Corporal é: {IMC}</p>
-                    <p>Você está com: Baixa peso</p>
-                </div>
-            )
-        } else if (IMC >= 0.185 && IMC <= 0.249) {
-            return (
-                <div>
-                    <p>O seu Indice de Massa Corporal é: {IMC}</p>
-                    <p>Você está com: Peso normal</p>
-                </div>
-            )
-        } else if (IMC > 0.249 && IMC <= 0.299) {
-            return (
-                <div>
-                    <p>O seu Indice de Massa Corporal é: {IMC}</p>
-                    <p>Você está com: Excesso de peso</p>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <p>O seu Indice de Massa Corporal é: {IMC}</p>
-                    <p>Você está com: Obesidade</p>
-                </div>
-            )
-        }
+    const calcIMC = (event) => {
+        event.preventDefault();
+        setIMC(((peso / Math.pow(altura, 2)) * 100).toFixed(2))
+        return IMC
     }
 
 
     return (
-        <div className={styles.div}>
-            <h1>Calculadora IMC</h1>
+        <div>
+            <h1 className={styles.title}>Calculadora IMC</h1>
 
             <div className={styles.divContent}>
 
-                <div className={styles.form}>
-                    <h2 className={styles.subTitle}>Calcular IMC</h2>
+                <div>
+                    <h2>Calcular IMC</h2>
                     <form>
                         <div className={styles.itemsForm}>
-                            <label>Peso:</label>
-                            <input type='number' placeholder='Digite o seu peso em KG' onChange={event => setPeso(parseFloat(event.target.value))} />
+                            <label>Peso</label>
+                            <input className={styles.inputForm} type='number' placeholder='Digite o seu peso em KG' onChange={event => setPeso(parseFloat(event.target.value))} />
                         </div>
                         <div className={styles.itemsForm}>
-                            <label>Altura:</label>
-                            <input type='number' placeholder='Digite a sua altura em CM' onChange={event => setAltura(parseFloat(event.target.value))} />
+                            <label>Altura</label>
+                            <input className={styles.inputForm} type='number' placeholder='Digite a sua altura em CM' onChange={event => setAltura(parseFloat(event.target.value))} />
                         </div>
                         <div className={styles.itemsForm}>
-                            <button className={styles.buttonForm}>Calcular IMC</button>
-                            <button className={styles.buttonForm}>Limpar valores</button>
+                            <button className={styles.buttonForm} onClick={calcIMC}>Calcular IMC</button>
                         </div>
-                        <div className={styles.itemsForm}>
+                        <div>
                             <h3>Resultado:</h3>
-                            {calcularIMC()}
-
+                            <p>O seu Indice de Massa Corporal é:<span className={styles.spanResult}> {IMC}</span></p>
+                            <p>Você está com:<span className={styles.spanResult}>
+                                {IMC < 0.185 ? ' Baixo peso' : IMC >= 0.185 && IMC <= 0.249 ? ' Peso normal'
+                                    : IMC > 0.249 && IMC <= 0.299 ? ' Excesso de peso' : ' Obesidade'}
+                            </span></p>
                         </div>
                     </form>
                 </div >
 
-                <div className={styles.tab}>
-                    <h2 className={styles.subTitle}>Classificação IMC</h2>
-                    <p>Cálculo: IMC = Peso (em kg) ÷ Altura² (em metros)</p>
-                    <p>Baixo peso: IMC MENOR(AJUSTAR) 18,5</p>
-                    <p>Peso normal: IMC ≥ 18,5 a 24,9</p>
-                    <p>Excesso de peso: IMC ≥ 25 a 29,9</p>
-                    <p>Obesidade: IMC ≥ 30</p>
+                <div>
+                    <h2 >Classificação IMC</h2>
+                    <p><span className={styles.spanResult}>Cálculo: IMC = Peso (em kg) ÷ Altura² (em metros)</span></p>
+                    <p>Baixo peso: IMC menor que 18,5</p>
+                    <p>Peso normal: IMC de 18,5 a 24,9</p>
+                    <p>Excesso de peso: IMC de 25 a 29,9</p>
+                    <p>Obesidade: IMC maior que 30</p>
                 </div>
 
             </div >
